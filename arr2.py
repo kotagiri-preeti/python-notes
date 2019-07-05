e86ffe2c518f8a7eb9efb6ed0f6ec1c970c4a974
kotagiri-preeti <50094402+kotagiri-preeti@users.noreply.github.com>
2019-07-05 07:03:25 +0530

    Add files via upload

---
+++ arr2.py
@@ -0,0 +1,12 @@
+
+from array import *
+arr=array('I',[])
+n=int(input("enter size of the array \n"))
+for i in range(n):
+    x=int(input("enter the value \n"))
+    arr.append(x)
+print(arr)
+for j in arr:
+    print(j)
+
+
